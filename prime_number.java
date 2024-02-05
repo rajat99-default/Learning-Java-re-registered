a5623ab065def4d2ac84b363ad58fbee7eb722cc
Rajat Sujane <rajatsujane6@gmail.com>
2024-02-05 20:08:14 +0530

    added 1 more program

--- prime_number.java
+++ prime_number.java
@@ -4,7 +4,21 @@ public class prime_number {
     public static void main(String[] args) {
         Scanner o  = new Scanner(System.in);
         int a = o.nextInt();
-        
+        int f=0;
+        for(int i = 1;i<=a;i++)
+        if(a%i==0)
+        {
+            f++;
+        }
+        if(f==2)
+        {
+            System.out.print("ths is prime");
+
+        }
+        else
+        {
+            System.out.print("not a prime");
+        }
     }
     
 }
